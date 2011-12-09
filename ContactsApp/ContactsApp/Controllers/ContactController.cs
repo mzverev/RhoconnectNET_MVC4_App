b79ee510e67def250b749c49e4a29d5d3de717c6
Maxim Zverev <max@rhomobile.com>
2011-12-09 15:17:02 -0800

    Initial commit

---
+++ ContactsApp/ContactsApp/Controllers/ContactController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContactApp.Models;
+
+namespace ContactsApp.Controllers
+{ 
+    public class ContactController : Controller
+    {
+        private ContactDBContext db = new ContactDBContext();
+
+        //
+        // GET: /Contact/
+
+        public ViewResult Index()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        //
+        // GET: /Contact/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            return View(contact);
+        }
+
+        //
+        // GET: /Contact/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Contact/Create
+
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");  
+            }
+
+            return View(contact);
+        }
+        
+        //
+        // GET: /Contact/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            return View(contact);
+        }
+
+        //
+        // POST: /Contact/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /Contact/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            return View(contact);
+        }
+
+        //
+        // POST: /Contact/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ContactsApp/ContactsApp/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace ContactApp.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Phone { get; set; }
+        public String Email { get; set; }
+    }
+
+    public class ContactDBContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
